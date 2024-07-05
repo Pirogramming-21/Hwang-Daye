c81562368c79a60c689d8d9d0e817c7d185c66ab
imyyye <daye2382@naver.com>
2024-07-05 23:35:19 +0900

    6단계 반복

--- python_problem.py/python_problem_1.py
+++ python_problem.py/python_problem_1.py
@@ -1,33 +1,37 @@
 num = 0
 count = 0
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
+while count <31:
+    while True:
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
 
-        if (num < 1) or (num > 3) :
-            print("1,2,3 중 하나를 입력하세요.")
-        else: 
-            break
-    except ValueError:
-        print("정수를 입력하세요")
+            if (num < 1) or (num > 3) :
+                print("1,2,3 중 하나를 입력하세요.")
+            else: 
+                break
+        except ValueError:
+            print("정수를 입력하세요")
 
-count+=num
+    for i in range (num):
+        count += 1
+        print("PlayerA:", count)
+        if count >= 31:
+            exit()
 
-for i in range (1, num+1):
-    print("playerA:",i)
+    while True:
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
 
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
-        
-        if (num < 1) or (num > 3) :
-            print("1,2,3 중 하나를 입력하세요.")
-        else: 
-            break
-    except ValueError:
-        print("정수를 입력하세요")
+            if (num < 1) or (num > 3) :
+                print("1,2,3 중 하나를 입력하세요.")
+            else: 
+                break
+        except ValueError:
+            print("정수를 입력하세요")
 
-count+=num
-
-for k in range (i+1,count+1):
-    print("playerB:",k)
+    for i in range (num):
+        count +=1
+        print("playerB:",count)
+    
+        if count >=31:
+            exit()
