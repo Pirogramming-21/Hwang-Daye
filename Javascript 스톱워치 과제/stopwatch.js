8051d00b6c60e4ce8ce41854ee6b20eee0488ae2
imyyye <daye2382@naver.com>
2024-07-06 04:59:30 +0900

    기록추가 기능

--- Javascript 스톱워치 과제/stopwatch.js
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -1,22 +1,26 @@
 let timerId;
-let time = 0; // 밀리초 단위로 변경
+let time = 0; 
 const stopwatch = document.getElementById("stopwatch");
+const lapTimes = document.getElementById("lapTimes");
 let sec, millisec;
+let lapRecords = []; 
 
 function printTime() {
-    time += 10; // 10밀리초씩 증가
+    time += 10; 
     stopwatch.innerText = getTimeFormatString();
 }
 
 function startClock() {
-    printTime();
-    stopClock();
-    timerId = setTimeout(startClock, 10); // 10밀리초 간격으로 변경
+    if (!timerId) { 
+        timerId = setInterval(printTime, 10); 
+    }
 }
 
 function stopClock() {
     if (timerId != null) {
-        clearTimeout(timerId);
+        clearInterval(timerId);
+        timerId = null; 
+        recordLapTime(); 
     }
 }
 
@@ -24,11 +28,29 @@ function resetClock() {
     stopClock();
     stopwatch.innerText = "00:00";
     time = 0;
+    lapRecords = []; /
+    updateLapTimes();
 }
 
 function getTimeFormatString() {
     sec = parseInt(String(time / 1000));
-    millisec = parseInt(String(time % 1000) / 10); // 밀리초를 10으로 나눠 2자리로 만듦
+    millisec = parseInt(String(time % 1000) / 10); 
 
     return String(sec).padStart(2, '0') + ":" + String(millisec).padStart(2, '0');
+}
+
+function recordLapTime() {
+    if (time > 0) { 
+        lapRecords.push(getTimeFormatString());
+        updateLapTimes();
+    }
+}
+
+function updateLapTimes() {
+    lapTimes.innerHTML = '';
+    lapRecords.forEach(lap => {
+        const li = document.createElement('li');
+        li.innerText = lap;
+        lapTimes.appendChild(li);
+    });
 }
