5f05d6a87895dfa80fedbc7fd94c6714fe6c704b
imyyye <daye2382@naver.com>
2024-07-06 01:03:22 +0900

    9단계 computer설정

--- python_problem.py/python_problem_1.py
+++ python_problem.py/python_problem_1.py
@@ -14,7 +14,7 @@ def brGame(player):
     elif player == "PlayerA":
         while True:
             try:
-                num = int(input(f"{player}: 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+                num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
                 if num < 1 or num > 3:
                     print("1, 2, 3 중 하나를 입력하세요.")
                 else:
