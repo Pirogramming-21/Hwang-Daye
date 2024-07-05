583d6584a9684bf58f826c675047639fee683f69
imyyye <daye2382@naver.com>
2024-07-05 23:55:43 +0900

    8단계 함수 생성

--- python_problem.py/python_problem_1.py
+++ python_problem.py/python_problem_1.py
@@ -1,9 +1,11 @@
 num = 0
 count = 0
-while count <31:
+    
+def brGame (player):
+    global count
     while True:
         try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
+            num = int(input(f"{player}: 부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
 
             if (num < 1) or (num > 3) :
                 print("1,2,3 중 하나를 입력하세요.")
@@ -14,26 +16,14 @@ while count <31:
 
     for i in range (num):
         count += 1
-        print("PlayerA:", count)
-        if count >= 31:
-            print("playerB wins!")
-            exit()
+        print(f"{player}:{count}")
 
-    while True:
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
-
-            if (num < 1) or (num > 3) :
-                print("1,2,3 중 하나를 입력하세요.")
-            else: 
-                break
-        except ValueError:
-            print("정수를 입력하세요")
-
-    for i in range (num):
-        count +=1
-        print("playerB:",count)
-    
-        if count >=31:
-            print("Player A wins!")
-            exit()
+while count <31:
+    brGame("PlayerA")
+    if count >= 31:
+        print("PlayerB wins!")
+        exit()
+    brGame("PlayerB")
+    if count >= 31:
+        print("PlayerA wins!")
+        exit()
