c1cc41f990197a20360a93f914b6ad7f0a89761d
imyyye <daye2382@naver.com>
2024-07-06 04:46:12 +0900

    스톱워치 기능

--- Javascript 스톱워치 과제/stopwatch.js
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -1,20 +1,18 @@
 let timerId;
-let time = 0;
+let time = 0; // 밀리초 단위로 변경
 const stopwatch = document.getElementById("stopwatch");
-let  hour, min, sec;
-
+let sec, millisec;
 
 function printTime() {
-    time++;
+    time += 10; // 10밀리초씩 증가
     stopwatch.innerText = getTimeFormatString();
 }
 
 function startClock() {
     printTime();
     stopClock();
-    timerId = setTimeout(startClock, 1000);
+    timerId = setTimeout(startClock, 10); // 10밀리초 간격으로 변경
 }
-
 
 function stopClock() {
     if (timerId != null) {
@@ -23,15 +21,14 @@ function stopClock() {
 }
 
 function resetClock() {
-    stopClock()
-    stopwatch.innerText = "00:00:00";
+    stopClock();
+    stopwatch.innerText = "00:00";
     time = 0;
 }
 
 function getTimeFormatString() {
-    hour = parseInt(String(time / (60 * 60)));
-    min = parseInt(String((time - (hour * 60 * 60)) / 60));
-    sec = time % 60;
+    sec = parseInt(String(time / 1000));
+    millisec = parseInt(String(time % 1000) / 10); // 밀리초를 10으로 나눠 2자리로 만듦
 
-    return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
+    return String(sec).padStart(2, '0') + ":" + String(millisec).padStart(2, '0');
 }
